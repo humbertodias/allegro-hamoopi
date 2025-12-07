026a28aa0a14d409b2bc64306d9badbb6e08d7c4
Humberto <hldtux@gmail.com>
2025-12-07 01:03:27 -0300

    ignore _codeql tmp files

--- _codeql_build_dir/CMakeFiles/HAMOOPI.dir/src/backend_manager.cpp.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/HAMOOPI.dir/src/backend_manager.cpp.o: \
- /home/runner/work/allegro-hamoopi/allegro-hamoopi/src/backend_manager.cpp \
- /usr/include/stdc-predef.h \
- /home/runner/work/allegro-hamoopi/allegro-hamoopi/src/backend_manager.h \
- /home/runner/work/allegro-hamoopi/allegro-hamoopi/src/graphics_backend.h \
- /home/runner/work/allegro-hamoopi/allegro-hamoopi/src/input_backend.h \
- /home/runner/work/allegro-hamoopi/allegro-hamoopi/src/audio_backend.h \
- /home/runner/work/allegro-hamoopi/allegro-hamoopi/src/system_backend.h
